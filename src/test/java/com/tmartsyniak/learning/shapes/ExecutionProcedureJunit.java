2c235ae68d1a471e8d7404f437f4e9db827f7e04
Taras Martsyniak <micton86@gmail.com>
2016-08-03 21:33:54 +0300

    check commit

---
+++ src/test/java/com/tmartsyniak/learning/shapes/ExecutionProcedureJunit.java
@@ -0,0 +1,7 @@
+package com.tmartsyniak.learning.shapes;
+
+/**
+ * Created by promedol on 02.08.16.
+ */
+public class ExecutionProcedureJunit {
+}
---
+++ src/test/java/com/tmartsyniak/learning/shapes/task1/FiguresSearchUtilTest.java
@@ -0,0 +1,7 @@
+package com.tmartsyniak.learning.shapes.task1;
+
+/**
+ * Created by promedol on 03.08.16.
+ */
+public class FiguresSearchUtilTest {
+}
